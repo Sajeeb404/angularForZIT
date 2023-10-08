6002cc68950a9cd3f2514ca35cad1240bf0e03f5
Md. Ashraful Islam <66846579+Sajeeb404@users.noreply.github.com>
2023-10-08 04:44:31 -0700

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ItemListComponent } from './pages/item-list/item-list.component';
+
+const routes: Routes = [
+
+  { path: 'itemList', component: ItemListComponent },
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
